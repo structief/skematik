ec915dd85453a48263234c56d9fa89f3584c2d5d
Koen <koen@beformed.be>
2018-01-14 13:07:03 +0100

    Create participant added
    - Dropdown link “Nieuwe deelnemer” works
    - Small fixes for images on homepage

--- images/public/application/components/backend/participants/participants.controller.js
+++ images/public/application/components/backend/participants/participants.controller.js
@@ -39,7 +39,8 @@ skematikControllers.controller('BeParticipantsController',["$scope", "$state", "
 	};
 
 	$scope.system = {
-		roles: ["student", "docent", "admin"]
+		roles: ["student", "docent", "admin"],
+		createParticipant: false
 	};
 
 	//Write something to fetch the participants dynamically.
@@ -47,8 +48,16 @@ skematikControllers.controller('BeParticipantsController',["$scope", "$state", "
 
 	//View functions
 	$scope.toggleDropdown = function($event){
-		angular.element($event.target).parent().parent().toggleClass("u__dropdown--show");
+		var el = findAncestor($event.target, "u__dropdown");
+		$(el).toggleClass("u__dropdown--show");
 	};
+	//Helper
+	var findAncestor = function (el, cls) {
+		while(!el.classList.contains(cls)){
+			el = el.parentElement;
+		}
+	    return el;
+	}
 
 	$scope.inRoles = function(roles, role){
 		return roles.indexOf(role) !== -1;
@@ -63,6 +72,10 @@ skematikControllers.controller('BeParticipantsController',["$scope", "$state", "
 	};
 
 	$scope.filterParticipants = function(participant){
+		//Is user a temporary (new) user?
+		if(participant.status == -1){
+			return true;
+		} 
 		//Check activity of participant
 		if((participant.status && $scope.filter.active) || (!participant.status && $scope.filter.inactive)){
 			//Check participant roles
@@ -70,6 +83,11 @@ skematikControllers.controller('BeParticipantsController',["$scope", "$state", "
 				if($scope.inRoles($scope.filter.roles, participant.roles[i])){
 					return true;
 				}
+			}
+
+			//If user does not have roles, but all roles are checked in the filter, just shown them already
+			if($scope.system.roles.length == $scope.filter.roles.length && participant.roles.length == 0){
+				return true;
 			}
 			return false;
 		}else{
@@ -92,5 +110,18 @@ skematikControllers.controller('BeParticipantsController',["$scope", "$state", "
 		//Show alert
 		$scope.participants.splice(index, 1);
 		$rootScope.$broadcast('alert.show', {title: "Deelnemer is verwijderd", message: "Klik <u>hier</u> om deze actie ongedaan te maken", type: "warning", clickCallback: undoDeletion}); 
+	}
+
+	$scope.addParticipant = function($event){
+		//Add new user
+		$scope.participants.push({"mail": null, "status": -1, "roles": []});
+
+		//Toggle dropdown
+		$scope.toggleDropdown($event);
+	}
+
+	$scope.saveParticipant = function($index){
+		//Change status to 1, that should do the trick
+		$scope.participants[$index]["status"] = 1;
 	}
 }]);
