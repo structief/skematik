cba1db67448d252a428babeb9c4f93f562dde070
Jan Everaert <EveraertJan@users.noreply.github.com>
2018-02-03 15:37:20 +0100

    Auth fix

--- images/api/src/helpers/auth.js
+++ images/api/src/helpers/auth.js
@@ -3,26 +3,31 @@ const config = require("./config.js")
 
 
 exports.checkToken = (min, pg, token, next, res) => {
-  const t = token.split(' ')[1];
+  console.log(min)
+  if(token) {
+    const t = token.split(' ')[1];
 
-  const decoded = jwt.decode(t, config.auth.secret);
-  let proceed = false;
+    const decoded = jwt.decode(t, config.auth.secret);
+    let proceed = false;
 
-  console.log(decoded.roles)
+    console.log(decoded.roles)
 
-  //check auth
-  for(let i = 0; i<decoded.roles.length; i++) {
-    if(parseInt(decoded.roles[i].permissions) >= parseInt(min) ) {
-      proceed = true;
+    //check auth
+    for(let i = 0; i<decoded.roles.length; i++) {
+      if(parseInt(decoded.roles[i].permissions) >= parseInt(min) ) {
+        proceed = true;
+      }
     }
-  }
 
-  if(decoded.expiresAt < new Date().getTime()) {
-    proceed =  false
-  } 
+    if(decoded.expiresAt < new Date().getTime()) {
+      proceed =  false
+    } 
 
-  if(proceed){
-    next(decoded);
+    if(proceed){
+      next(decoded);
+    } else {
+      res.send(401)
+    }
   } else {
     res.send(401)
   }
