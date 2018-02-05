9c99440ce99f0f642f544491219170b043f6a0db
Koen <koen@beformed.be>
2018-02-05 23:16:54 +0100

    Fixed stepwizard and added feedback to input
    - Still some work to be done though

--- images/public/application/components/frontend/register/register.controller.js
+++ images/public/application/components/frontend/register/register.controller.js
@@ -1,7 +1,37 @@
 skematikControllers.controller('RegisterController',["$scope", "$state", "$stateParams", "$rootScope", function($scope, $stateProvider, $stateParams, $rootScope) {
-	$scope.activeStep = 0;
+	$scope.wizard = {
+		"step": {
+			"max": 3,
+			"active": 1,
+			"showErrors": false,
+		}
+	};
 
-	$scope.nextStep = function(){
-		$scope.activeStep++;
+	$scope.new = {
+		"organisation": {
+			"name" : null
+		},
+		"account": {
+			"mail": null,
+			"password": null
+		}
+	};
+
+	$scope.nextStep = function(required_elements){
+		for(var i=0;i<required_elements.length;i++){
+			if(required_elements[i] == null){
+				$scope.wizard.step.showErrors = true;
+				return;
+			}
+		}
+		$scope.wizard.step.showErrors = false;
+		$scope.wizard.step.active++;
+	}
+	$scope.prevStep = function(){
+		$scope.wizard.step.active--;
+	}
+
+	$scope.register = function(){
+		console.log($scope.new);
 	}
 }]);
