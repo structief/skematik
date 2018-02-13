3538903779460db344412bfa86891459ccd0584b
Jan Everaert <EveraertJan@users.noreply.github.com>
2018-02-13 22:15:25 +0100

    answer update (los op den develop)

--- images/api/src/Answers.js
+++ images/api/src/Answers.js
@@ -56,8 +56,89 @@ class Answers {
 
       // add to answers
       if( go ) {
-        await pg("answers").insert(insert).then(function(id) {
-          res.send(200)
+        await pg("answers").insert(insert).then(async(id) => {
+          
+
+
+          let result = {};
+          const rowstructure = [];
+
+
+          let answers = [];
+
+          await pg.select().table("answers").where({tableID: req.params.uuid}).then(function(a) {
+            answers = a;
+          });
+
+
+
+          await pg.select()
+            .table("schema")
+            .where({"schema.uuid": req.params.uuid})
+            .join('cells', 'schema.id', "=", "cells.tableID")
+            .then( function (r) {
+              if(r.length > 0) {
+
+                result["uuid"] = req.params.uuid;
+                result["title"] = r[0].title;
+                const temp = [];
+                Object.keys(r[0].headers).map((key, index) => {
+                  temp.push(key);
+                });
+                result["headers"] = temp;
+
+
+                result["created_at"] = r[0].created_at;
+                result["created_at"] = r[0].created_at;
+                result["updated_at"] = r[0].updated_at;
+
+
+                Object.keys(r[0].rows).map((key, index) => {
+                  const found = [];
+                  for (let i = 0; i < r.length; i++) {
+                    if (r[i]["row"] === key) {
+                      const num = answers.filter(answer => answer.cellID === r[i].uuid);
+                      found.push({
+                        max: r[i].max,
+                        current: num,
+                        col: r[i].col,
+                        uuid: r[i].uuid
+                      })
+                    }
+                  }
+
+                  const intermediary = [];
+
+                  for(let i = 0; i < temp.length; i++) {
+                    for(let j = 0; j < found.length; j++) {
+                      if(found[j].col === temp[i]) {
+                        intermediary.push(found[j])
+                      }
+                    }
+                  }
+
+                  rowstructure.push({
+                    name: key,
+                    cells: intermediary
+                  });
+                });
+
+
+                result["rows"] = rowstructure;
+
+                result["answers"] = answers;
+                res.send(result);
+              } else {
+                res.sendStatus(404);
+
+              }
+            }).then(function() {
+
+          }).catch(function(error) {
+            res.sendStatus(404);
+          })
+
+
         }).catch(function(error) {
           res.send("error" + error)
         })
