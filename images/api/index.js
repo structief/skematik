b67d0769b6228d209207b8ed9ee14cf7b5c21867
Koen <koen@beformed.be>
2018-03-07 11:39:27 +0100

    Removed testing lines
    ..forgot them, although I clearly wrote “Don’t forget!”

--- images/api/index.js
+++ images/api/index.js
@@ -3,14 +3,4 @@
 
 const App = require('./App');
 
-//return new App().start();
-
-//Testing purposes, don't forget to remove!
-const consumers = './src/consumers/';
-const emitter = require('./src/helpers/emitter.js');
-const fs = require('fs');
-
-//Load consumers
-fs.readdirSync(consumers).forEach(file => {
-  require(consumers + file);
-});
+return new App().start();
