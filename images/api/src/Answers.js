b3d6415af5d0e8ef694e985967a073219e11909a
Koen <koen@beformed.be>
2018-03-19 19:32:29 +0100

    Don’t add participations if one of them failed

--- images/api/src/Answers.js
+++ images/api/src/Answers.js
@@ -30,116 +30,123 @@ class Answers {
         var participation = req.body["participations"][i];
         var timestamp = new Date();
 
-        insert.push({
-          "cellID": participation["cell"]["uuid"],
-          "tableID": req.params.uuid,
-          "participant": req.body["participant"],
-          "created_at": timestamp,
-          "updated_at": timestamp
-        });
-
-        await pg.update({'current': pg.raw('current + 1')}).table('cells').where({uuid: participation["cell"]['uuid']}).then(function(v) {
-        });
-
-        // Asses if can register
+        // Assess if can register
         await pg.select().table('answers').where({ cellID: participation["cell"]['uuid']}).join('cells', 'cells.uuid', '=', 'answers.cellID').then(function(d) {
           if(d.length > 0 && d.length >= d[0].max) {
             //do not allow
             return res.send(417, { message: 'no more place', cell: {uuid: participation["cell"]['uuid']}});
+          }else{
+            //Add participation for insertion
+            insert.push({
+              "cellID": participation["cell"]["uuid"],
+              "tableID": req.params.uuid,
+              "participant": req.body["participant"],
+              "created_at": timestamp,
+              "updated_at": timestamp
+            });
           }
         });
       }
 
-      // add to answers
-      await pg("answers").insert(insert).then(async(id) => {
+      // add to answers, if there isn't already a 417 underway
+      if(insert.length > 0 && !res.headersSent){
+        //Up counter for each insertion
+        for(var i=0;i<insert.length;i++){
+          await pg.update({'current': pg.raw('current + 1')}).table('cells').where({uuid: insert[i]['cellID']}).then(function(v) {
+          });
+        }
         
+        //Add entire stuff to db
+        await pg("answers").insert(insert).then(async(id) => {
+          
 
 
-        let result = {};
-        const rowstructure = [];
+          let result = {};
+          const rowstructure = [];
 
 
-        let answers = [];
+          let answers = [];
 
-        await pg.select().table("answers").where({tableID: req.params.uuid}).then(function(a) {
-          answers = a;
-        });
-
-
-        //Get scheme again
-        await pg.select()
-          .table("schema")
-          .where({"schema.uuid": req.params.uuid})
-          .join('cells', 'schema.id', "=", "cells.tableID")
-          .then( function (r) {
-            if(r.length > 0) {
-
-              result["uuid"] = req.params.uuid;
-              result["title"] = r[0].title;
-              const temp = [];
-              Object.keys(r[0].headers).map((key, index) => {
-                temp.push(key);
-              });
-              result["headers"] = temp;
+          await pg.select().table("answers").where({tableID: req.params.uuid}).then(function(a) {
+            answers = a;
+          });
 
 
-              result["created_at"] = r[0].created_at;
-              result["created_at"] = r[0].created_at;
-              result["updated_at"] = r[0].updated_at;
+          //Get scheme again
+          await pg.select()
+            .table("schema")
+            .where({"schema.uuid": req.params.uuid})
+            .join('cells', 'schema.id', "=", "cells.tableID")
+            .then( function (r) {
+              if(r.length > 0) {
+
+                result["uuid"] = req.params.uuid;
+                result["title"] = r[0].title;
+                const temp = [];
+                Object.keys(r[0].headers).map((key, index) => {
+                  temp.push(key);
+                });
+                result["headers"] = temp;
 
 
-              Object.keys(r[0].rows).map((key, index) => {
-                const found = [];
-                for (let i = 0; i < r.length; i++) {
-                  if (r[i]["row"] === key) {
-                    const num = answers.filter(answer => answer.cellID === r[i].uuid);
-                    found.push({
-                      max: r[i].max,
-                      current: num,
-                      col: r[i].col,
-                      uuid: r[i].uuid
-                    })
-                  }
-                }
+                result["created_at"] = r[0].created_at;
+                result["created_at"] = r[0].created_at;
+                result["updated_at"] = r[0].updated_at;
 
-                const intermediary = [];
 
-                for(let i = 0; i < temp.length; i++) {
-                  for(let j = 0; j < found.length; j++) {
-                    if(found[j].col === temp[i]) {
-                      intermediary.push(found[j])
+                Object.keys(r[0].rows).map((key, index) => {
+                  const found = [];
+                  for (let i = 0; i < r.length; i++) {
+                    if (r[i]["row"] === key) {
+                      const num = answers.filter(answer => answer.cellID === r[i].uuid);
+                      found.push({
+                        max: r[i].max,
+                        current: num,
+                        col: r[i].col,
+                        uuid: r[i].uuid
+                      })
                     }
                   }
-                }
 
-                rowstructure.push({
-                  name: key,
-                  cells: intermediary
+                  const intermediary = [];
+
+                  for(let i = 0; i < temp.length; i++) {
+                    for(let j = 0; j < found.length; j++) {
+                      if(found[j].col === temp[i]) {
+                        intermediary.push(found[j])
+                      }
+                    }
+                  }
+
+                  rowstructure.push({
+                    name: key,
+                    cells: intermediary
+                  });
                 });
-              });
 
 
-              result["rows"] = rowstructure;
+                result["rows"] = rowstructure;
 
-              result["answers"] = answers;
+                result["answers"] = answers;
 
-              // Subscription worked, fire event
-              // Replace 'mail' by the selected method by the admin
-              // But this is just a first setup
-              emitter.emit("mail.subscription.added", {insert});
+                // Subscription worked, fire event
+                // Replace 'mail' by the selected method by the admin
+                // But this is just a first setup
+                emitter.emit("mail.subscription.added", {insert});
 
-              res.send(result);
-            } else {
+                res.send(result);
+              } else {
+                res.sendStatus(404);
+              }
+            })
+            .then(function() {})
+            .catch(function(error) {
               res.sendStatus(404);
-            }
-          })
-          .then(function() {})
-          .catch(function(error) {
-            res.sendStatus(404);
-          })
-      }).catch(function(error) {
-        return res.send("error" + error)
-      })
+            })
+        }).catch(function(error) {
+          return res.send("error" + error)
+        })
+      }
     })
   }
 }
